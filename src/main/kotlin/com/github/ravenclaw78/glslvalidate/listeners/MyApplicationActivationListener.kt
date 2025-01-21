c6d0f0ab7b8feb8e6b118726eef79012be615138
GitHub Action <action@github.com>
2025-01-21 18:18:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/ravenclaw78/glslvalidate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ravenclaw78/glslvalidate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ravenclaw78.glslvalidate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
